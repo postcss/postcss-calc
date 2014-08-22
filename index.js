22bc35c5fea9fce3bdea281234f6ad54b5fb782c
Maxime Thirouin <m@moox.io>
2014-08-22 06:07:56 +0200

    expose less stuff (this was stupid)

--- index.js
+++ index.js
@@ -4,36 +4,23 @@
 var reduceCSSCalc = require("reduce-css-calc")
 
 /**
- * Expose plugin & helper
- */
-module.exports = plugin
-module.exports.transformDecl = transformDecl
-
-/**
  * PostCSS plugin to reduce calc() function calls.
  */
-function plugin() {
+module.exports = function plugin() {
   return function(style) {
-    style.eachDecl(transformDecl)
-  }
-}
+    style.eachDecl(function transformDecl(dec) {
+      if (!dec.value) {
+        return
+      }
 
-/**
- * Reduce CSS calc on a declaration object
- *
- * @param {object} dec [description]
- */
-function transformDecl(dec) {
-  if (!dec.value) {
-    return
-  }
-
-  try {
-    dec.value = transform(dec.value)
-  }
-  catch (err) {
-    err.position = dec.position
-    throw err
+      try {
+        dec.value = transform(dec.value)
+      }
+      catch (err) {
+        err.position = dec.position
+        throw err
+      }
+    })
   }
 }
 
