c75eee9ac038b31fe6e652edc4e3c8a5f461d2f2
Ludovico Fischer <livrerie@gmail.com>
2020-11-24 18:22:32 +0100

    refactor: switch to OnceExit() API.

--- src/index.js
+++ src/index.js
@@ -8,20 +8,25 @@ function pluginCreator(opts) {
     mediaQueries: false,
     selectors: false
   }, opts);
+
   return {
     postcssPlugin: 'postcss-calc',
-    Declaration(node, {result}) {
-      transform(node, "value", options, result);
-    },
-    AtRule(node, {result}) {
-      if (options.mediaQueries) {
-        transform(node, "params", options, result);
-      }
-    },
-    Rule(node, {result}) {
-      if (options.selectors) {
-        transform(node, "selector", options, result);
-      }
+    OnceExit(css, { result }) {
+      css.walk(node => {
+        const { type } = node;
+
+        if (type === 'decl') {
+          transform(node, "value", options, result);
+        }
+        
+        if (type === 'atrule' && options.mediaQueries) {
+          transform(node, "params", options, result);
+        }
+
+        if (type === 'rule' && options.selectors) {
+          transform(node, "selector", options, result);
+        }
+      });
     }
   }
 }
