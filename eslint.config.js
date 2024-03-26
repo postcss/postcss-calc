e507b9e56bdabca501dfb68f194afd9eb7d7c2f7
Ludovico Fischer <livrerie@gmail.com>
2024-03-26 17:31:25 +0100

    chore: commit an ESLint configuration to the repository

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const js = require('@eslint/js');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['src/parser.js'],
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    rules: {
+      curly: 'error',
+    },
+  },
+];
