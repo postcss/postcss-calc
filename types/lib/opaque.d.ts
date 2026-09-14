e6f3e32ca2e627a05c459054c51777de090113f3
Ludovico Fischer <livrerie@gmail.com>
2026-09-14 14:09:41 +0200

    refactor: improve serializer readability

--- types/lib/opaque.d.ts
+++ types/lib/opaque.d.ts
@@ -4,6 +4,6 @@ export type Node = import('./node.js').Node;
 export type OpaqueComponent = import('./node.js').OpaqueComponent;
 /** @param {OpaqueComponent[]} components @param {(node: Node) => Node} simplify @return {OpaqueComponent[]} */
 declare function simplifyComponents(components: OpaqueComponent[], simplify: (node: Node) => Node): OpaqueComponent[];
-/** @param {OpaqueComponent[]} components @param {(node: Node) => string} serialize @return {string} */
-declare function serializeComponents(components: OpaqueComponent[], serialize: (node: Node) => string): string;
+/** @param {OpaqueComponent[]} components @param {string[]} buffer @param {(node: Node, buffer: string[]) => void} serialize @return {void} */
+declare function serializeComponents(components: OpaqueComponent[], buffer: string[], serialize: (node: Node, buffer: string[]) => void): void;
 export { simplifyComponents, serializeComponents };
